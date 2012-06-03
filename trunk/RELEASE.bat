03c72eb70d6e4bc1d764d70d97697f98412a7e3e
kboronka@gmail.com <kboronka@gmail.com@d2cee243-c52e-aa50-d3d6-2b9144403790>
2012-06-03 15:00:58 +0000

    updated RELEASE.bat

--- trunk/RELEASE.bat
+++ trunk/RELEASE.bat
@@ -1,70 +1,74 @@
 @echo off
 pushd "%~dp0"
-
 set SOLUTION=ChessCup.sln
+set BASEURL=https://chesscup.googlecode.com/svn/
 set CONFIG=Release
 
-set BITS=x86
-if "%PROCESSOR_ARCHITECTURE%" == "AMD64" set BITS=x64
-if "%PROCESSOR_ARCHITEW6432%" == "AMD64" set BITS=x64
+:: Paths
+	set MSBUILD="%WinDir%\Microsoft.NET\Framework\v2.0.50727\msbuild.exe"
+	set REPLACE="release\sar.exe"
+	set ZIP="%PROGRAMFILES%\7-Zip\7zG.exe" a -tzip
 
-IF %BITS% == x86 (
-	rem OS is 32bit
-	set NSIS="%PROGRAMFILES%\NSIS\makensis.exe" /V3
-) ELSE (
-	rem OS is 64bit
-	set NSIS="%PROGRAMFILES(X86)%\NSIS\makensis.exe" /V3
-)
+	set BITS=x86
+	if "%PROCESSOR_ARCHITECTURE%" == "AMD64" set BITS=x64
+	if "%PROCESSOR_ARCHITEW6432%" == "AMD64" set BITS=x64
 
-set ZIP="%PROGRAMFILES%\7-Zip\7zG.exe" a -tzip
-set MSBUILD="%WinDir%\Microsoft.NET\Framework\v2.0.50727\msbuild.exe"
-set REPLACE="lib\sar\sar.exe"
+	IF %BITS% == x86 (
+		rem OS is 32bit
+		set MAKENSIS="%PROGRAMFILES%\NSIS\makensis.exe" /V3
+	) ELSE (
+		rem OS is 64bit
+		set MAKENSIS="%PROGRAMFILES(X86)%\NSIS\makensis.exe" /V3
+	)
+
+:: Build Soultion
+	echo "VERSION.MAJOR.MINOR.BUILD".
+	set /p VERSION="> "
+
+	svn update
+	%REPLACE% ChessCup.nsi "0.0.0.0" "%VERSION%"
+	%REPLACE% AssemblyInfo.cs "0.0.0.0" "%VERSION%"
+	%REPLACE% %SOLUTION% "Format Version 10.00" "Format Version 9.00"
+	%REPLACE% %SOLUTION% "Visual Studio 2008" "Visual Studio 2005"
+
+	echo building chesscup
+	%MSBUILD% "src\chesscup.sln" /p:Configuration=%CONFIG% /p:Platform="Any CPU"
+	if errorlevel 1 goto BuildFailed
+
+	echo creating installer
+	%NSIS% "src\Installer\chesscup.nsi"
+	if errorlevel 1 goto BuildFailed
+	move "src\Installer\ChessCup %VERSION% Install.exe" "ChessCup %VERSION% Install.exe"
 
 
-echo VERSION.MAJOR.MINOR.BUILD
-set /p VERSION="> "
+:: Build Complete
+	%REPLACE% ChessCup.nsi "%VERSION%" "0.0.0.0"
+	%REPLACE% AssemblyInfo.cs "%VERSION%" "0.0.0.0"
+	%REPLACE% %SOLUTION% "Format Version 9.00" "Format Version 10.00"
+	%REPLACE% %SOLUTION% "Visual Studio 2005" "Visual Studio 2008"
+	
+	svn ci
+	svn cp %BASEURL%/trunk BASEURL/tags/%VERSION%  # tag trunk as %VERSION%
+	pause
 
-%REPLACE% ChessCup.nsi "0.0.0.0" "%VERSION%"
-%REPLACE% AssemblyInfo.cs "0.0.0.0" "%VERSION%"
-%REPLACE% %SOLUTION% "Format Version 10.00" "Format Version 9.00"
-%REPLACE% %SOLUTION% "Visual Studio 2008" "Visual Studio 2005"
+	echo
+	echo
+	echo build completed
 
-echo building chesscup
-%MSBUILD% "src\chesscup.sln" /p:Configuration=%CONFIG% /p:Platform="Any CPU"
-if errorlevel 1 goto BuildFailed
+	popd
+	exit /b 0
 
-echo creating installer
-%NSIS% "src\Installer\chesscup.nsi"
-if errorlevel 1 goto BuildFailed
-move "src\Installer\ChessCup %VERSION% Install.exe" "ChessCup %VERSION% Install.exe"
+:: Build Failed
+	:BuildFailed
+	%REPLACE% ChessCup.nsi "%VERSION%" "0.0.0.0"
+	%REPLACE% AssemblyInfo.cs "%VERSION%" "0.0.0.0"
+	%REPLACE% ChessCup.sln "Format Version 9.00" "Format Version 10.00"
+	%REPLACE% ChessCup.sln "Visual Studio 2005" "Visual Studio 2008"
 
-rem -----------------------------------------------------------------------
-rem Build Complete
-rem -----------------------------------------------------------------------
+	echo
+	echo
+	echo build failed
+	pause
 
-%REPLACE% ChessCup.nsi "%VERSION%" "0.0.0.0"
-%REPLACE% AssemblyInfo.cs "%VERSION%" "0.0.0.0"
-%REPLACE% %SOLUTION% "Format Version 9.00" "Format Version 10.00"
-%REPLACE% %SOLUTION% "Visual Studio 2005" "Visual Studio 2008"
-
-echo build completed
-
-popd
-exit /b 0
-
-rem -----------------------------------------------------------------------
-rem Build Failed
-rem -----------------------------------------------------------------------
-
-:BuildFailed
-
-%REPLACE% ChessCup.nsi "%VERSION%" "0.0.0.0"
-%REPLACE% AssemblyInfo.cs "%VERSION%" "0.0.0.0"
-%REPLACE% ChessCup.sln "Format Version 9.00" "Format Version 10.00"
-%REPLACE% ChessCup.sln "Visual Studio 2005" "Visual Studio 2008"
-
-echo build failed
-pause
-
-popd
-exit /b 1
+	popd
+	exit /b 1
