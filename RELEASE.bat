98d25c7c8566da79e6351b73cf3d3a0d0e1d81d4
kboronka@gmail.com <kboronka@gmail.com@d2cee243-c52e-aa50-d3d6-2b9144403790>
2013-01-27 15:03:05 +0000

    updated build script

--- RELEASE.bat
+++ RELEASE.bat
@@ -1,23 +1,26 @@
-:: Development Enviorment
-::
-:: HTML workshop							http://www.microsoft.com/en-us/download/details.aspx?id=21138
-:: Microsoft.NET v2.0.50727					http://www.microsoft.com/download/en/details.aspx?id=19
-:: SharpDevelop v3.2.1.6466					http://sourceforge.net/projects/sharpdevelop/files/SharpDevelop%203.x/3.2/SharpDevelop_3.2.1.6466_Setup.msi/download
-:: TortoiseSVN 1.7.4(+command line tools)	https://sourceforge.net/projects/tortoisesvn/files/1.7.4/Application/
-:: 7zip										http://www.7-zip.org/download.html
+:SoftwareRequired
+	:: Microsoft.NET v2.0						http://www.filehippo.com/download_dotnet_framework_2/
+	:: Microsoft.NET v3.5						http://www.filehippo.com/download_dotnet_framework_3/
+	:: TortoiseSVN (+command line tools)		http://www.filehippo.com/download_tortoisesvn/
+	:: 7zip 32bit								http://www.filehippo.com/download_7zip_32/
+	:: 7zip 64bit								http://www.filehippo.com/download_7-zip_64/
+	:: NSIS										http://www.filehippo.com/download_nsis/
+	:: SharpDevelop v3.2.1.6466					http://sourceforge.net/projects/sharpdevelop/files/SharpDevelop%203.x/3.2/SharpDevelop_3.2.1.6466_Setup.msi/download
+	:: HTML workshop							http://www.microsoft.com/en-us/download/details.aspx?id=21138
 
-@echo off
-pushd "%~dp0"
-set SOLUTION=ChessCup.sln
-set INSTALLER=ChessCup.nsi
-set REPO=https://chesscup.googlecode.com/svn
-set CONFIG=Release
+:BuildEnvironment
+	@echo off
+	pushd "%~dp0"
+	set SOLUTION=ChessCup.sln
+	set INSTALLER=ChessCup.nsi
+	set REPO=https://chesscup.googlecode.com/svn
+	set CONFIG=Release
 
-:: Paths
+:Paths
 	set SAR="lib\sar\sar.exe"
 	set ZIP="%PROGRAMFILES%\7-Zip\7zG.exe" a -tzip
 
-:: Build Soultion
+:Build
 	echo "VERSION.MAJOR.MINOR.BUILD".
 	set /p VERSION="> "
 
@@ -25,45 +28,31 @@ set CONFIG=Release
 
 	%SAR% -r AssemblyInfo.* ((Version)\(\"\d+\.\d+\.\d+\.\d+\"\)) "Version(\"%VERSION%\")"
 	%SAR% -r %INSTALLER% ((PRODUCT_VERSION)\s\"\d+\.\d+\.\d+\.\d+\") "PRODUCT_VERSION \"%VERSION%\""
-	%SAR% -r %SOLUTION% "Format Version 10.00" "Format Version 9.00"
-	%SAR% -r %SOLUTION% "Visual Studio 2008" "Visual Studio 2005"
 
 	echo building chesscup
-	%SAR% -b.net 2.0 %SOLUTION% /p:Configuration=%CONFIG% /p:Platform=\"Any CPU\"
+	%SAR% -b.net 3.5 %SOLUTION% /p:Configuration=%CONFIG% /p:Platform=\"Any CPU\"
 	if errorlevel 1 goto BuildFailed
 
 	echo creating installer
 	%SAR% -b.nsis src\Installer\%INSTALLER%
 	if errorlevel 1 goto BuildFailed
 	
-:: Build Complete
+:BuildComplete
 	move "src\Installer\ChessCup %VERSION% Install.exe" "ChessCup %VERSION% Install.exe"
 	copy src\ChessCup\bin\%CONFIG%\*.exe release\*.exe
 	copy src\ChessCup\bin\%CONFIG%\*.dll release\*.dll
 	copy license.txt release\license.txt
 	
-	%SAR% -r %SOLUTION% "Format Version 9.00" "Format Version 10.00"
-	%SAR% -r %SOLUTION% "Visual Studio 2005" "Visual Studio 2008"
-	
 	svn commit -m "version %VERSION%"
 	svn copy %REPO%/trunk %REPO%/tags/%VERSION% -m "Tagging the %VERSION% version release of the project"
-
-	echo
-	echo
+	svn update
+	
 	echo build completed, trunk has been tagged
-
 	popd
 	exit /b 0
 
-:: Build Failed
-	:BuildFailed
-	%SAR% -r ChessCup.sln "Format Version 9.00" "Format Version 10.00"
-	%SAR% -r ChessCup.sln "Visual Studio 2005" "Visual Studio 2008"
-
-	echo
-	echo
+:BuildFailed
 	echo build failed
 	pause
-
 	popd
 	exit /b 1
