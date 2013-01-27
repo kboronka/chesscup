381c69b78f1de9373c29b63899a0ae671d915010
kboronka@gmail.com <kboronka@gmail.com@d2cee243-c52e-aa50-d3d6-2b9144403790>
2013-01-27 15:22:06 +0000

    updated skylib library calls

--- src/ChessCup/GUI/ChessCup.cs
+++ src/ChessCup/GUI/ChessCup.cs
@@ -21,7 +21,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
-using SkylaLib.Tools;
+using skylib.Tools;
 
 using ChessCup.Game;
 using ChessCup.Game.Units;
--- src/ChessCup/Program.cs
+++ src/ChessCup/Program.cs
@@ -19,7 +19,7 @@
 using System;
 using System.Windows.Forms;
 
-using SkylaLib.Tools;
+using skylib.Tools;
 
 namespace ChessCup
 {
